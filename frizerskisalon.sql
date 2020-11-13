0abea9a5ff72c96cf5ed6fb57f78035d246b7a3b
Velimir Vujičić <velimir.vujicic@hotmail.com>
2020-11-13 22:39:44 +0100

    Povezivanje frizerski salon

--- frizerskisalon.sql
+++ frizerskisalon.sql
@@ -14,7 +14,8 @@ create table djelatnica(
 create table korisnik(
     sifra int not null primary key auto_increment,
     ime varchar (50) not null,
-    prezime varchar (50) not null
+    prezime varchar (50) not null,
+    djelatnica int
 );
 
 create table usluga (
@@ -22,3 +23,17 @@ create table usluga (
     naziv varchar (20) not null,
     cijena decimal (15,5)
 );
+
+create table posjeta (
+    sifra int not null primary key auto_increment,
+    datumtermina datetime not null,
+    trajanje int not null,
+    korisnik int,
+    usluga int
+);
+
+alter table korisnik add foreign key (djelatnica) references djelatnica (sifra);
+
+alter table posjeta add foreign key (korisnik) references korisnik (sifra);
+
+alter table posjeta add foreign key (usluga) references usluga (sifra);
