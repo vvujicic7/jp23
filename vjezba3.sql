6f8667ba608393549c4f1b9528ef51e501d58110
Velimir Vujičić <velimir.vujicic@hotmail.com>
2020-11-16 21:50:11 +0100

    vjezba3

---
+++ vjezba3.sql
@@ -0,0 +1,11 @@
+# c:\xampp\mysql\bin\mysql -uedunova -pedunova < d:\vjezba3.sql
+
+drop database if exists vjezba3;
+create database vjezba3;
+
+use vjezba3;
+
+create table programski jezik(
+	id int not null primary key auto_increment,
+	java 
+);
