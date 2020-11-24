60ac0ed9cc3fa6c105c03fce1c1fa93b4e6c843c
Velimir Vujičić <velimir.vujicic@hotmail.com>
2020-11-24 20:58:11 +0100

    vjezba2_2_update

--- vjezba2_2.sql
+++ vjezba2_2.sql
@@ -33,11 +33,46 @@ alter table zupanija add foreign key (zupan) references zupan(sifra);
 alter table opcina add foreign key (zupanija) references zupanija(sifra);
 alter table mjesto add foreign key (opcina) references opcina(sifra);
 
-select * from zupanija;
 select * from zupan;
-insert into zupan (ime,prezime) 
-	values
+insert into zupan (ime,prezime) values
+	('Dubrovacki','Dub'),
+	('Osijecki','Osi'),
+	('Zagrebacki','Zag');
+
+select * from zupanija;
 insert into zupanija (naziv,zupan) values
 	('dubrovacko_neretvanska',1),
-	('osijecko_baranjska',1),
-	('grad_zagreb',1);
+	('osijecko_baranjska',2),
+	('grad_zagreb',3);
+	
+select * from opcina;
+insert into opcina(zupanija,naziv) values
+	(1,'Dubrovnik'),
+	(1,'Cavtat'),
+	(2,'Osijek'),
+	(2,'Nasice'),
+	(3,'Zagreb'),
+	(3,'Samobor');
+	
+select * from mjesto;
+insert into mjesto (opcina,naziv) values
+	(1,'Konavle'),
+	(1,'Ston'),
+	(2,'Slano'),
+	(2,'Srebreno'),
+	(3,'Antunovac'),
+	(3,'Bilje'),
+	(4,'Bizovac'),
+	(4,'Ceminac'),
+	(5,'Zapresic'),
+	(5,'Jastrebarsko'),
+	(6,'Zabok'),
+	(6,'Svetanedelja');
+	
+update mjesto set naziv='Cavtat' where sifra=1;
+update mjesto set naziv='Mljet' where sifra=2;
+update mjesto set naziv='Zelina' where sifra=9;
+update mjesto set naziv='Valpovo' where sifra=6;
+update mjesto set naziv='Djakovo' where sifra=7;
+
+
