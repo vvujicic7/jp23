f0f9b5d37d2bb2c9304f6daa04b31272ca5544d2
Velimir Vujičić <velimir.vujicic@hotmail.com>
2020-12-14 18:57:49 +0100

    eclipse

---
+++ eclipse/Dobar dan Edunova/src/edunova/edunova1.java
@@ -0,0 +1,8 @@
+package edunova;
+
+public class edunova1 {
+	public static void main(String[] args) {
+		System.out.println("Edunova");
+	}
+
+}
---
+++ eclipse/HelloWorld/src/edunova/HelloWorld.java
@@ -0,0 +1,13 @@
+package edunova;
+
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		
+	}
+	
+	
+}
