e552dc3f9ef19caf1befbaccd999b683ecdebfd4
Velimir Vujičić <velimir.vujicic@hotmail.com>
2020-12-16 19:05:41 +0100

    1612

--- eclipse/Edunova01/src/edunova/E06Vjezbanje.java
+++ eclipse/Edunova01/src/edunova/E06Vjezbanje.java
@@ -1,10 +1,14 @@
 package edunova;
 
+import javax.swing.JOptionPane;
+
 public class E06Vjezbanje {
 
 	// Korisnik unosi decimalni broj
 	// program ispisuje samo cijeli dio broja
 	public static void main(String[] args) {
+		String b=JOptionPane.showInputDialog("broj");
 		
+		System.out.println(b);
 	}
 }
