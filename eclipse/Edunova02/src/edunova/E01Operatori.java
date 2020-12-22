c37bd0bf49b8841e7995d47acd960683f5cf2b94
Velimir Vujičić <velimir.vujicic@hotmail.com>
2020-12-22 19:13:15 +0100

    2212

--- eclipse/Edunova02/src/edunova/E01Operatori.java
+++ eclipse/Edunova02/src/edunova/E01Operatori.java
@@ -16,6 +16,30 @@ public class E01Operatori {
 		i=9%2;
 		System.out.println(i);
 		
+		rezultat=rezultat+1;
+		rezultat+=1; // isti učinak kao linija iznad
+		
+		rezultat=rezultat+i;
+		rezultat+=i; // isti učinak kao linija iznad
+		System.out.println("---------");
+		// ista pravila vrijede i za ostale operatore u kombinaciji...
+		
+		// ++ --
+		
+		// uvećaanje za 1
+		i=i+1; // 1.način
+		i+=1; // drugi način
+		System.out.println(i);
+		i++; // treći način -prvo se koristi vrijednost pa se uveća
+		System.out.println(i);
+		
+		++i; // prvo uveća pa se koristi vrijednost
+		System.out.println(i);
+		
+		i=1;
+		System.out.println(i++); //2
+		System.out.println(++i); //3
+		System.out.println(i); //3
 	}
 
 }
