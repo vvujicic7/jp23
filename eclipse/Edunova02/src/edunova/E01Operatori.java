bf2b98eb177c4cdb835398ec50c4f76704dffb67
Velimir Vujičić <velimir.vujicic@hotmail.com>
2020-12-28 19:27:45 +0100

    2812

--- eclipse/Edunova02/src/edunova/E01Operatori.java
+++ eclipse/Edunova02/src/edunova/E01Operatori.java
@@ -40,6 +40,19 @@ public class E01Operatori {
 		System.out.println(i++); //2
 		System.out.println(++i); //3
 		System.out.println(i); //3
+		
+		
+		i=i-1;
+		i-=1;
+		i--;
+		
+		i=1;
+			
+		System.out.println(i--); // 0
+		System.out.println(--i); // -1
+		System.out.println(i);	 // -1
+			
+			
 	}
 
 }
