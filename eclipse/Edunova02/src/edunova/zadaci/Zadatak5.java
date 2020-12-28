d5dde9251becd10023308508bfaa3e5d93347df6
Velimir Vujičić <velimir.vujicic@hotmail.com>
2020-12-28 18:25:03 +0100

    2812

---
+++ eclipse/Edunova02/src/edunova/zadaci/Zadatak5.java
@@ -0,0 +1,11 @@
+package edunova.zadaci;
+
+public class Zadatak5 {
+	
+	// Korisnik unosi cijeli broj
+	// Ako je paran broj unesen ispiši Osijek
+	// Ako je neparan broj unesen ispiši Zagreb
+	
+	// koristiti inline if
+
+}
