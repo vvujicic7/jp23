bc6402c072d8c4eb0aced7b7a3b0d2e213d1e25e
Velimir Vujičić <velimir.vujicic@hotmail.com>
2020-12-29 23:45:10 +0100

    2912

---
+++ eclipse/Edunova02/src/edunova/zadaci/Zadatak6.java
@@ -0,0 +1,10 @@
+package edunova.zadaci;
+
+public class Zadatak6 {
+	
+	// Korisnik unosi 2 broja. 
+	// Ukoliko je produkt ta dva broja paran broj
+	// ispisuje razliku unesinih brojeva
+	// inače ispisuje cijeli dio kvocjenta prvog i drugog broja
+
+}
