a79604a338d7a540b1d97710d634f5dd214b8919
Velimir Vujičić <velimir.vujicic@hotmail.com>
2020-12-29 23:46:06 +0100

    2912

---
+++ eclipse/Edunova02/src/edunova/zadaci/Zadatak7.java
@@ -0,0 +1,9 @@
+package edunova.zadaci;
+
+public class Zadatak7 {
+	
+	// Program od korisnika unosi slovo
+	// u slučaju slova A ispisuje 1. mjesto
+	// pa sve do slova E gdje ispisuje 5. mjesto
+
+}
