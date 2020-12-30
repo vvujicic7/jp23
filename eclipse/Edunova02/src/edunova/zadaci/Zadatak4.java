2a4bdc01e85a1b0548f3dc318e858371fd3b299b
Velimir Vujičić <velimir.vujicic@hotmail.com>
2020-12-30 20:02:42 +0100

    3012

--- eclipse/Edunova02/src/edunova/zadaci/Zadatak4.java
+++ eclipse/Edunova02/src/edunova/zadaci/Zadatak4.java
@@ -1,7 +1,32 @@
 package edunova.zadaci;
 
+import javax.swing.JOptionPane;
+
 public class Zadatak4 {
 
+	public static void main(String[] args) {
+		
+		String b1 = JOptionPane.showInputDialog("broj");
+		String b2 = JOptionPane.showInputDialog("broj");
+		String b3 = JOptionPane.showInputDialog("broj");
+		
+		int i = Integer.parseInt(b1);
+		int j = Integer.parseInt(b2);
+		int k = Integer.parseInt(b3);
+		
+		if(i>=j & i>=k) {
+			System.out.println(i);
+		}
+		
+		if(j>i & j>k) {
+			System.out.println(j);
+		}
+		
+		if(k>j & k>i) {
+			System.out.println(k);
+		}
+		}
 	// Korisnik unosi 3 cijela broja 
 	// program ispisuje najveći
 }
+
