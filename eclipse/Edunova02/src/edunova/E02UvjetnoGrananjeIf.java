3c847082f263ec2a48c835a0fda8e59a0c330461
Velimir Vujičić <velimir.vujicic@hotmail.com>
2020-12-30 20:19:16 +0100

    3012

--- eclipse/Edunova02/src/edunova/E02UvjetnoGrananjeIf.java
+++ eclipse/Edunova02/src/edunova/E02UvjetnoGrananjeIf.java
@@ -1,6 +1,6 @@
 package edunova;
 
-public class E03UvjetnoGrananjeIf {
+public class E02UvjetnoGrananjeIf {
 	public static void main(String[] args) {
 		
 	
--- eclipse/Edunova02/src/edunova/zadaci/Zadatak5.java
+++ eclipse/Edunova02/src/edunova/zadaci/Zadatak5.java
@@ -1,6 +1,15 @@
 package edunova.zadaci;
 
+import javax.swing.JOptionPane;
+
 public class Zadatak5 {
+	public static void main(String[] args) {
+		
+		String b1 = JOptionPane.showInputDialog("unesi cijeli broj");
+		
+		int b2 = Integer.parseInt(b1);
+		System.out.println(b2%2 ==0?  "Osijek" : "Zagreb");
+	}
 	
 	// Korisnik unosi cijeli broj
 	// Ako je paran broj unesen ispiši Osijek
