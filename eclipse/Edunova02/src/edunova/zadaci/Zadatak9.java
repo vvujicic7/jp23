aad99e87fe6bcfc207d0e2c2e5922b3b10313c62
Velimir Vujičić <velimir.vujicic@hotmail.com>
2020-12-30 22:15:06 +0100

    3012

---
+++ eclipse/Edunova02/src/edunova/zadaci/Zadatak9.java
@@ -0,0 +1,17 @@
+package edunova.zadaci;
+
+public class Zadatak9 {
+
+	// Program unosi 3 znaka. Program ispisuje OK ako je niz od tri unesena znaka palindrom
+	
+	public static void main(String[] args) {
+		char pz='A';
+		char dz='N';
+		char tz='A';
+		
+		if(pz==tz) {
+			System.out.println("OK");
+		}
+		
+	}
+}
