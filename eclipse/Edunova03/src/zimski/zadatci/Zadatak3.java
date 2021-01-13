4a6984d6cc3780b2877bab336ab302d4350f6c85
Velimir Vujičić <velimir.vujicic@hotmail.com>
2021-01-13 12:56:12 +0100

    1301

--- eclipse/Edunova03/src/zimski/zadatci/Zadatak3.java
+++ eclipse/Edunova03/src/zimski/zadatci/Zadatak3.java
@@ -1,18 +1,31 @@
 package zimski.zadatci;
 
-import java.util.Scanner;
 
 public class Zadatak3 {
 	// Kreirati program koji za dva unesena broja ispisuje sve prim/prosti/prime brojeve između njih.
 	
 	public static void main(String[] args) {
 		
-		Scanner ulaz = new Scanner(System.in);
+		int i = 0;
+		int broj = 0;
 		
-		System.out.println("Unesi dva broja: ");
+		// prazan String
+		String primeBrojevi = "";
 		
-		
-		// ne valja...nastavi (java.util??
+		for (i = 1; i <= 100; i++) {
+			int zbroj = 0;
+			for(broj = i; broj >= 1; broj--) {
+				if(i%broj == 0) {
+					zbroj = zbroj + 1;
+				}
+			}
+			if (zbroj == 2) {
+				primeBrojevi = primeBrojevi + i + " ";
+			}
+		}
+		System.out.println("Prime brojevi od 1 do 100 su :");
+		System.out.println(primeBrojevi);
+	
 		
 	}
 
