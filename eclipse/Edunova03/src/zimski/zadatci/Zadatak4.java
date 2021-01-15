f53567dba8815a5f9091e602d3719e299b2a4907
Velimir Vujičić <velimir.vujicic@hotmail.com>
2021-01-15 18:13:58 +0100

    Zadatak4

--- eclipse/Edunova03/src/zimski/zadatci/Zadatak4.java
+++ eclipse/Edunova03/src/zimski/zadatci/Zadatak4.java
@@ -1,7 +1,25 @@
 package zimski.zadatci;
 
+import java.util.Scanner;
+
 public class Zadatak4 {
 	// Kreirati program koji unosi dvije matrice 4x4 te ispisuje njihov zbroj
+	public static void main(String[] args) {
+		
+	int a,b,c,d, zbroj = 0, e,f,g;
 	
+	Scanner ulaz = new Scanner(System.in);
+	
+	System.out.println("Unesi broj redaka i stupaca prve matrice");
+	a = ulaz.nextInt();
+	b = ulaz.nextInt();
+	
+	// nije gotovo
+	
+	
+	
+	
+
+	}
 
 }
