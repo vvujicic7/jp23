a76064ef1e6670e420b38621280eaded49409b69
Velimir Vujičić <velimir.vujicic@hotmail.com>
2021-03-26 12:59:58 +0100

    Uredjivanje Izbornika

--- netbeans/HotelZavrsniRad/src/main/java/hotel/view/Izbornik.java
+++ netbeans/HotelZavrsniRad/src/main/java/hotel/view/Izbornik.java
@@ -27,57 +27,101 @@ public class Izbornik extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jMenu1 = new javax.swing.JMenu();
+        jToolBar1 = new javax.swing.JToolBar();
+        lblVrijeme = new javax.swing.JLabel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jmProgrami = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
+        jMenuItem4 = new javax.swing.JMenuItem();
+        jMenuItem5 = new javax.swing.JMenuItem();
+        jmOnama = new javax.swing.JMenu();
+
+        jMenu1.setText("jMenu1");
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jToolBar1.setRollover(true);
+        jToolBar1.setMaximumSize(new java.awt.Dimension(48, 16));
+        jToolBar1.setMinimumSize(new java.awt.Dimension(48, 16));
+        jToolBar1.setPreferredSize(new java.awt.Dimension(48, 16));
+
+        lblVrijeme.setText("Vrijeme");
+        jToolBar1.add(lblVrijeme);
+
+        jmProgrami.setText("Programi");
+        jmProgrami.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jmProgramiActionPerformed(evt);
+            }
+        });
+
+        jMenuItem1.setText("Boravci");
+        jmProgrami.add(jMenuItem1);
+
+        jMenuItem2.setText("Gosti");
+        jmProgrami.add(jMenuItem2);
+
+        jMenuItem3.setText("Usluge");
+        jmProgrami.add(jMenuItem3);
+
+        jMenuItem4.setText("Djelatnici");
+        jmProgrami.add(jMenuItem4);
+
+        jMenuItem5.setText("Izlaz");
+        jmProgrami.add(jMenuItem5);
+
+        jMenuBar1.add(jmProgrami);
+
+        jmOnama.setText("O nama");
+        jmOnama.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jmOnamaMouseClicked(evt);
+            }
+        });
+        jMenuBar1.add(jmOnama);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jToolBar1, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 253, Short.MAX_VALUE)
+                .addComponent(jToolBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Izbornik.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Izbornik.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Izbornik.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Izbornik.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+    private void jmProgramiActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmProgramiActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jmProgramiActionPerformed
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Izbornik().setVisible(true);
-            }
-        });
-    }
+    private void jmOnamaMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jmOnamaMouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jmOnamaMouseClicked
+
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JMenuItem jMenuItem4;
+    private javax.swing.JMenuItem jMenuItem5;
+    private javax.swing.JToolBar jToolBar1;
+    private javax.swing.JMenu jmOnama;
+    private javax.swing.JMenu jmProgrami;
+    private javax.swing.JLabel lblVrijeme;
     // End of variables declaration//GEN-END:variables
 }
